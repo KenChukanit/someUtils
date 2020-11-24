75325cbfb11409ce4fd32d07e47f1dfae50839c4
KenChukanit <chonlasek.c@gmail.com>
2020-11-24 15:14:45 -0800

    add file

---
+++ utils.js
@@ -0,0 +1,14 @@
+function product(a,b) {
+    let c = a+b;
+    return c;
+}
+
+function divide(a,b) {
+    let c = a/b;
+    return c;
+}
+
+function power(a,b){
+    let c =a**b;
+    return c;
+}
